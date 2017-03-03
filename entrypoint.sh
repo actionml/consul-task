074adcc8b4a93012125a2cec0f1cc6064dbb0e53
Denis Baryshev <dennybaa@gmail.com>
2017-03-03 18:45:51 +0300

    aws cli ~/.aws directory autoconfig

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 runas=$(id -u $USER):$(id -g $USER)
 volumes="/config /apps"
 
-# set up volume and run command as unprivileged user
+# Set up volumes
 chown $runas $volumes && chmod 755 $volumes
+
+# AWS cli setup: check if volume has aws cli configuration if so link it
+if [ ! -e /home/$USER/.aws ] && [ -d /config/.aws ]; then
+    cp -r /config/.aws /home/$USER
+    chown -R $runas /home/$USER/.aws
+fi
+
+# Run command as unprivileged user
 exec gosu $runas $@
